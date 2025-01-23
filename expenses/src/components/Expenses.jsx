099155f4de5860fbf5bac92ea5359c8e4734ed0e
robinristo <robin-risto.kivit@voco.ee>
2025-01-23 15:34:21 +0200

    7. Komponentide kompositsioon ja info edastamine

--- expenses/src/components/Expenses.jsx
+++ expenses/src/components/Expenses.jsx
@@ -1,13 +1,14 @@
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem.jsx';
+import Card from './Card';
 
 const Expenses = (props) => {
 
     return (
-        <div className='expenses'>
+        <Card className='expenses'>
             <ExpenseItem data={props.expenses[0]}/>
             <ExpenseItem data={props.expenses[1]}/>
-        </div>
+        </Card>
     )
 }
 
