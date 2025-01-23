9e18aedb442f5e85cb5ce21a1e617e4336b145c9
robinristo <robin-risto.kivit@voco.ee>
2025-01-23 14:09:58 +0200

    5. Ülesanne 1 - kujunduse lisamine komponendile

--- expenses/src/components/ExpenseDate.jsx
+++ expenses/src/components/ExpenseDate.jsx
@@ -1,3 +1,5 @@
+import './ExpenseDate.css';
+
 const ExpenseDate = (props) => {
 
     const day = props.date.toLocaleString('en-US', {day: '2-digit'});
@@ -5,10 +7,10 @@ const ExpenseDate = (props) => {
     const year = props.date.getFullYear();
 
     return (
-        <div>
-            <div>{month}</div>
-            <div>{day}</div>
-            <div>{year}</div>
+        <div className='expense-date'>
+            <div className='expense-date__month'>{month}</div>
+            <div className='expense-date__day'>{day}</div>
+            <div className='expense-date__year'>{year}</div>
         </div>
     );
 }
