6e2d74dc76f2a4b60ec3cb43b13b944b66e6bbc6
robinristo <robin-risto.kivit@voco.ee>
2025-01-24 13:33:20 +0200

    11.4. Vormi andmete saatmise töötlus

--- expenses/src/components/NewExpense/ExpenseForm.jsx
+++ expenses/src/components/NewExpense/ExpenseForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './ExpenseForm.css'
+import ExpenseDate from '../Expenses/ExpenseDate';
 
 const ExpenseForm = (props) => {
     // const [userInput, setUserInput] = useState({
@@ -42,26 +43,41 @@ const ExpenseForm = (props) => {
         setEnteredDate(event.target.value);
     }
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            price: enteredPrice,
+            date: new Date(enteredDate)
+        }
+        setEnteredTitle('');
+        setEnteredPrice('');
+        setEnteredDate('');
+    }
+
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
                     <input 
                         type="text" 
-                        onChange={titleChangeHandler} 
+                        onChange={titleChangeHandler}
+                        value={enteredTitle}
                     />
                 </div>
                 <div className='new-expense__control'>
                     <label>Price</label>
                     <input type="number" min="0.01" step="0.01" 
                         onChange={priceChangeHandler}
+                        value={enteredPrice}
                     />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
                     <input type="date" min="2024-11-12" max="2026-01-31" 
                         onChange={dateChangeHandler}
+                        value={enteredDate}
                     />
                 </div>
             </div>
