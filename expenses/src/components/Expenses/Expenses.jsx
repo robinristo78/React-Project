d4efccd0278f0e0855d878294ccd280ffa37479c
robinristo <robin-risto.kivit@voco.ee>
2025-02-05 09:55:41 +0200

    12. Ülesanne 4 - sisekomponendi andmete suunamine välikomponendile

--- expenses/src/components/Expenses/Expenses.jsx
+++ expenses/src/components/Expenses/Expenses.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem.jsx';
 import Card from '../UI/Card.jsx';
@@ -5,11 +6,17 @@ import ExpensesFilter from './ExpensesFilter.jsx';
 
 const Expenses = (props) => {
 
+    const [filteredYear, setFilteredYear] = useState('2024');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+        console.log(`Your data in Expenses.js ${selectedYear}`);
+    }
+
     return (
         <Card className='expenses'>
-            <ExpensesFilter/> 
-            {/* siin peab ExpenseFilter-ist tulema andmed Expenses.jsx,
-            siis peab console.log(`Year data in Expenses.jsx ${aasta}`) olema */}
+            <ExpensesFilter selectedYear={filteredYear} onChangeFilter={filterChangeHandler} /> 
+            
             <ExpenseItem data={props.expenses[0]}/>
             <ExpenseItem data={props.expenses[1]}/>
         </Card>
--- expenses/src/components/Expenses/ExpensesFilter.jsx
+++ expenses/src/components/Expenses/ExpensesFilter.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import './ExpensesFilter.css';
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
+
+    const dropdownChangeHandler = (event) => {
+        props.onChangeFilter(event.target.value);
+    }
+
     return (
         <div className='expenses-filter'>
             <div className='expenses-filter__control'>
                 <label>Filter by year</label>
-                <select>
+                <select value={props.selectedYear} onChange={dropdownChangeHandler}>
                     <option value='2023'>2023</option>
                     <option value='2024'>2024</option>
                     <option value='2025'>2025</option>
